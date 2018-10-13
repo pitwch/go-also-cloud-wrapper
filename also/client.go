142999f371b611c983391c02342446182f7a14d3
SP <admin@pitw.ch>
2018-10-13 22:18:26 +0200

    Inital

---
+++ also/client.go
@@ -0,0 +1,434 @@
+package also
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+//Version of Wrapper
+const (
+	Version = "1.0.0"
+)
+
+// DefaultHTTPTransport is an http.RoundTripper that has DisableKeepAlives set true.
+var DefaultHTTPTransport = &http.Transport{
+	DisableKeepAlives: true,
+}
+
+// DefaultHTTPClient is an http.Client with the DefaultHTTPTransport and (Cookie) Jar set nil.
+var DefaultHTTPClient = &http.Client{
+	Jar:       nil,
+	Transport: DefaultHTTPTransport,
+}
+
+//Client Struct
+type Client struct {
+	restURL      *url.URL
+	Username     string
+	Password     string
+	option       *Options
+	client       *http.Client
+	sessiontoken *string
+}
+
+//Login Struct
+type LoginStruct struct {
+	Username string `json:"username"`
+	Password string `json:"passwort"`
+}
+
+//Error Struct
+type ErrorStruct struct {
+	Type    string `json:"Type"`
+	Message string `json:"Message"`
+	Fields  []struct {
+		Reason  string `json:"Reason"`
+		Name    string `json:"Name"`
+		Message string `json:"Message"`
+	} `json:"Fields"`
+}
+
+//Building new Client
+func NewClient(RestURL string, apiUser string, apiPassword string, apiDatabase string, apiModule []string, options *Options) (*Client, error) {
+	restURL, err := url.Parse(RestURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if options == nil {
+		options = &Options{}
+	}
+
+	if options.APIPrefix == "" {
+		options.APIPrefix = "/SimpleAPI/SimpleAPIService.svc/rest/"
+	}
+
+	if options.LoginEndpoint == "" {
+		options.LoginEndpoint = "GetSessionToken"
+	}
+
+	if options.UserAgent == "" {
+		options.UserAgent = "go-also-cloud-wrapper " + Version
+	}
+
+	//Set default batchsize for batch requests
+	if options.Batchsize == 0 {
+		options.Batchsize = 200
+	}
+
+	if DefaultHTTPClient == nil {
+
+		DefaultHTTPClient = http.DefaultClient
+	}
+
+	path := options.APIPrefix + "/"
+	restURL.Path = path
+
+	return &Client{
+		restURL:      restURL,
+		Username:     apiUser,
+		Password:     apiPassword,
+		option:       options,
+		client:       DefaultHTTPClient,
+		sessiontoken: nil,
+	}, nil
+}
+
+//Function for creating new sessiontoken
+func (c *Client) createNewsessiontoken(ctx context.Context) (sessionid string, err error) {
+
+	//Check URL, else exit
+	_, err = url.ParseRequestURI(c.restURL.String())
+	if err != nil {
+		return "", fmt.Errorf("URL in wrong format: %s", err)
+	}
+
+	//Build Login URL
+	urlstr := c.restURL.String() + c.option.LoginEndpoint
+
+	//Create Login Body
+	data := LoginStruct{c.Username, c.Password}
+
+	//Encode Login Body to JSON
+	body := new(bytes.Buffer)
+	encoder := json.NewEncoder(body)
+	if err := encoder.Encode(data); err != nil {
+		return "", err
+	}
+
+	//Build Login Request
+	req, err := http.NewRequest("POST", urlstr, body)
+	if err != nil {
+		return "", err
+	}
+
+	//Set Login Header
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	//If Response gives errors print also Body
+	if resp.StatusCode == http.StatusBadRequest ||
+		resp.StatusCode == http.StatusNotFound ||
+		resp.StatusCode == http.StatusMethodNotAllowed ||
+		resp.StatusCode == http.StatusInternalServerError {
+
+		return "", errorFormatterPx(ctx, c, resp.StatusCode, resp.Body)
+	}
+	//If everything OK just return sessiontoken
+	return resp.Header.Get("sessiontoken"), nil
+
+}
+
+//Function for Login
+//Helper Function for Login to PROFFIX REST-API
+//Login is automatically done
+func (c *Client) Login(ctx context.Context) error {
+
+	// If sessiontoken doesnt yet exists create a new one
+	if c.sessiontoken == nil || *c.sessiontoken == "" {
+		sessionid, err := c.createNewsessiontoken(ctx)
+		c.sessiontoken = &sessionid
+		return err
+		// If sessiontoken already exists return stored value
+	} else {
+		return nil
+	}
+}
+
+//LOGOUT Request for REST-API
+//Accepts PxSession ID as Input or if left empty uses SessionId from active Session
+//Returns Statuscode,error
+func (c *Client) Logout(ctx context.Context) (int, error) {
+	//Just logout if we have a valid sessiontoken
+	if c.sessiontoken != nil {
+
+		//Delete Login Object from PROFFIX REST-API
+		_, _, statuscode, err := c.request(ctx, "DELETE", c.option.LoginEndpoint, url.Values{}, nil)
+
+		//Set sessiontoken to nil
+		c.sessiontoken = nil
+
+		return statuscode, err
+	} else {
+		return 0, nil
+	}
+
+}
+
+//Request Method
+//Building the Request Method for Client
+func (c *Client) request(ctx context.Context, method, endpoint string, params url.Values, data interface{}) (io.ReadCloser, http.Header, int, error) {
+
+	var urlstr string
+
+	if params.Encode() != "" {
+		urlstr = c.restURL.String() + endpoint + "?" + params.Encode()
+
+	} else {
+		urlstr = c.restURL.String() + endpoint
+	}
+
+	//If Log enabled log URL
+	if c.sessiontoken != nil {
+		logDebug(ctx, c, fmt.Sprintf("Request-URL: %v, Method: %v, PxSession-ID: %v", urlstr, method, *c.sessiontoken))
+	} else {
+		logDebug(ctx, c, fmt.Sprintf("Request-URL: %v, Method: %v, PxSession-ID: none", urlstr, method))
+	}
+
+	switch method {
+	case http.MethodPost, http.MethodPut:
+	case http.MethodDelete, http.MethodGet, http.MethodOptions:
+	default:
+		return nil, nil, 0, fmt.Errorf("Method is not recognised: %s", method)
+	}
+
+	body := new(bytes.Buffer)
+	encoder := json.NewEncoder(body)
+	if err := encoder.Encode(data); err != nil {
+		return nil, nil, 0, fmt.Errorf("JSON Encoding failed: %s", err)
+	}
+
+	req, err := http.NewRequest(method, urlstr, body)
+	req = req.WithContext(ctx)
+
+	if err != nil {
+		return nil, nil, 0, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	if c.sessiontoken != nil {
+		req.Header.Set("Authenticate", "CCPSessionId "+*c.sessiontoken)
+
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, nil, resp.StatusCode, err
+	}
+
+	return resp.Body, resp.Header, resp.StatusCode, err
+
+}
+
+//POST Request for REST-API
+//Accepts Context, Endpoint and Data as Input
+//Returns io.ReadCloser,http.Header,Statuscode,error
+func (c *Client) Post(ctx context.Context, endpoint string, data interface{}) (io.ReadCloser, http.Header, int, error) {
+	err := c.Login(ctx)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	request, header, statuscode, err := c.request(ctx, "POST", endpoint, url.Values{}, data)
+
+	//If Log enabled in options log data
+	logDebug(ctx, c, fmt.Sprintf("Sent data in POST-Request: %v", data))
+
+	//If Login is invalid - try again
+	if statuscode == 401 {
+		//Get new sessiontoken and write to var
+		err := c.Login(ctx)
+		request, header, statuscode, err := c.request(ctx, "POST", endpoint, url.Values{}, data)
+
+		return request, header, statuscode, err
+	}
+
+	//If Statuscode not 201
+	if statuscode != 201 {
+		return request, header, statuscode, errorFormatterPx(ctx, c, statuscode, request)
+	}
+
+	//Write the latest sessiontoken back to var
+	newsession := header.Get("sessiontoken")
+	if newsession != "" {
+		*c.sessiontoken = newsession
+	}
+
+	return request, header, statuscode, err
+}
+
+//PUT Request for REST-API
+//Accepts Endpoint and Data as Input
+//Returns io.ReadCloser,http.Header,Statuscode,error
+func (c *Client) Put(ctx context.Context, endpoint string, data interface{}) (io.ReadCloser, http.Header, int, error) {
+	err := c.Login(ctx)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	request, header, statuscode, err := c.request(ctx, "PUT", endpoint, url.Values{}, data)
+
+	//If Log enabled in options log data
+	logDebug(ctx, c, fmt.Sprintf("Sent data in PUT-Request: %v", data))
+
+	//If Login is invalid - try again
+	if statuscode == 401 {
+		//Get new sessiontoken and write to var
+		err := c.Login(ctx)
+
+		request, header, statuscode, err := c.request(ctx, "PUT", endpoint, url.Values{}, data)
+
+		return request, header, statuscode, err
+	}
+
+	//If Statuscode not 204
+	if statuscode != 204 {
+		return request, header, statuscode, errorFormatterPx(ctx, c, statuscode, request)
+	}
+
+	//Write the latest sessiontoken back to var
+	*c.sessiontoken = header.Get("sessiontoken")
+	//c.Logout(sessiontoken)
+
+	return request, header, statuscode, err
+}
+
+//GET Request for REST-API
+//Accepts Endpoint and url.Values as Input
+//Returns io.ReadCloser,http.Header,Statuscode,error
+func (c *Client) Get(ctx context.Context, endpoint string, params url.Values) (io.ReadCloser, http.Header, int, error) {
+
+	err := c.Login(ctx)
+
+	if err != nil {
+		return nil, nil, 0, err
+	}
+
+	request, header, statuscode, err := c.request(ctx, "GET", endpoint, params, nil)
+
+	//If Login is invalid - try again
+	if statuscode == 401 {
+		//Get new sessiontoken and write to var
+		err := c.Login(ctx)
+
+		request, header, statuscode, err := c.request(ctx, "GET", endpoint, params, nil)
+
+		return request, header, statuscode, err
+	}
+
+	//If Statuscode not 200
+	if statuscode != 200 {
+		return request, header, statuscode, errorFormatterPx(ctx, c, statuscode, request)
+	}
+
+	//Write the latest sessiontoken back to var
+	*c.sessiontoken = header.Get("sessiontoken")
+
+	return request, header, statuscode, err
+}
+
+//DELETE Request for REST-API
+//Accepts Endpoint and url.Values as Input
+//Returns io.ReadCloser,http.Header,Statuscode,error
+func (c *Client) Delete(ctx context.Context, endpoint string) (io.ReadCloser, http.Header, int, error) {
+	err := c.Login(ctx)
+	if err != nil {
+		return nil, nil, 0, err
+	}
+	request, header, statuscode, err := c.request(ctx, "DELETE", endpoint, nil, nil)
+
+	//If Login is invalid - try again
+	if statuscode == 401 {
+		//Get new sessiontoken and write to var
+		err := c.Login(ctx)
+		request, header, statuscode, err := c.request(ctx, "DELETE", endpoint, nil, nil)
+
+		return request, header, statuscode, err
+	}
+
+	//If Statuscode not 204
+	if statuscode != 204 {
+		return request, header, statuscode, errorFormatterPx(ctx, c, statuscode, request)
+	}
+
+	//Write the latest sessiontoken back to var
+	*c.sessiontoken = header.Get("sessiontoken")
+
+	return request, header, statuscode, err
+}
+
+//INFO Request for REST-API
+//Accepts Webservice Key as Input or if left empty uses key from options parameter
+//Returns Info about API as io.ReadCloser,error
+func (c *Client) Info(ctx context.Context, pxapi string) (io.ReadCloser, error) {
+
+	param := url.Values{}
+
+	//If no Key submitted in Function use Options
+	if pxapi == "" {
+		//Set Key from Options
+		param.Set("key", c.option.Key)
+	} else {
+		param.Set("key", pxapi)
+	}
+
+	request, _, _, err := c.request(ctx, "GET", "PRO/Info", param, "")
+
+	return request, err
+}
+
+func errorFormatterPx(ctx context.Context, c *Client, statuscode int, request io.Reader) (err error) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(request)
+	errbyte := buf.Bytes()
+	errstr := buf.String()
+
+	//Do Forced Logout...
+	logoutstatus, err := c.Logout(ctx)
+
+	logDebug(ctx, c, fmt.Sprintf("Logout after Error with Status: %v", logoutstatus))
+
+	//Define Error Struct
+	parsedError := ErrorStruct{}
+
+	//Try to parse JSON in ErrorStruct
+	err = json.Unmarshal(errbyte, &parsedError)
+
+	//If error on parse return plain text
+	if err != nil {
+		return fmt.Errorf("ERROR: %v", errstr)
+	}
+	if len(parsedError.Fields) > 0 {
+		var errorFields []string
+		for _, field := range parsedError.Fields {
+			errorFields = append(errorFields, field.Name)
+		}
+		return fmt.Errorf("Status: %v \n Type %s: %s\n Fields: %v", statuscode, parsedError.Type, parsedError.Message, errorFields)
+
+	}
+	return fmt.Errorf("Status: Statuscode %v\n Type %s: %s", statuscode, parsedError.Type, parsedError.Message)
+}
+
+func logDebug(ctx context.Context, c *Client, logtext string) {
+	//If Log enabled in options
+	if c.option.Log == true {
+		log.Print(logtext)
+	}
+}
---
+++ also/options.go
@@ -0,0 +1,16 @@
+package also
+
+import (
+	"time"
+)
+
+type Options struct {
+	Key           string
+	APIPrefix     string
+	LoginEndpoint string
+	UserAgent     string
+	Timeout       time.Duration
+	VerifySSL     bool
+	Batchsize     int
+	Log           bool
+}
